001c0a3fc920672f47767d807791400085b27cd9
Zhuoran Wang <Z.Wang-102@student.tudelft.nl>
2024-02-10 14:14:33 +0100

    lab1 complete

--- user/xargs.c
+++ user/xargs.c
@@ -100,4 +100,63 @@ enum state transform_state(enum state cur, enum char_type ct)
 
 int main(int argc, char *argv[])
 {
+      if (argc - 1 >= MAXARG) {
+    fprintf(2, "xargs: too many arguments.\n");
+    exit(1);
+  }
+  char lines[MAXSZ];
+  char *p = lines;
+  char *x_argv[MAXARG] = {0}; // 参数指针数组，全部初始化为空指针
+
+  // 存储原有的参数
+  for (int i = 1; i < argc; ++i) {
+    x_argv[i - 1] = argv[i];
+  }
+  int arg_beg = 0;          // 参数起始下标
+  int arg_end = 0;          // 参数结束下标
+  int arg_cnt = argc - 1;   // 当前参数索引
+  enum state st = S_WAIT;   // 起始状态置为S_WAIT
+
+  while (st != S_END) {
+    // 读取为空则退出
+    if (read(0, p, sizeof(char)) != sizeof(char)) {
+      st = S_END;
+    } else {
+      st = transform_state(st, get_char_type(*p));
+    }
+
+    if (++arg_end >= MAXSZ) {
+      fprintf(2, "xargs: arguments too long.\n");
+      exit(1);
+    }
+
+    switch (st) {
+    case S_WAIT:          // 这种情况下只需要让参数起始指针前移
+      ++arg_beg;
+      break;
+    case S_ARG_END:       // 参数结束，将参数地址存入x_argv数组中
+      x_argv[arg_cnt++] = &lines[arg_beg];
+      arg_beg = arg_end;
+      *p = '\0';          // 替换为字符串结束符
+      break;
+    case S_ARG_LINE_END:  // 将参数地址存入x_argv数组中同时执行指令
+      x_argv[arg_cnt++] = &lines[arg_beg];
+      // 不加break，因为后续处理同S_LINE_END
+    case S_LINE_END:      // 行结束，则为当前行执行指令
+      arg_beg = arg_end;
+      *p = '\0';
+      if (fork() == 0) {
+        exec(argv[1], x_argv);
+      }
+      arg_cnt = argc - 1;
+      clearArgv(x_argv, arg_cnt);
+      wait(0);
+      break;
+    default:
+      break;
+    }
+
+    ++p;    // 下一个字符的存储位置后移
+  }
+  exit(0);
 }
